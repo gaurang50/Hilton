54ff5c2b42c101c4053dc020494def9d6fc7e844
gaurang50 <paliwalgaurang763@gmail.com>
2024-04-23 18:27:16 +0530

    Web Socket

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import HiltonSocket from './HiltonSocket'; 
+
+function App() {
+  return (
+    <div className="App">
+      
+      <HiltonSocket /> 
+    </div>
+  );
+}
+
+export default App;
+
